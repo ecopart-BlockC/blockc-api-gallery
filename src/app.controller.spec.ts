87d248b0c329683f6147bdaeb55b00e3ddcc8400
Jessica Lopes Duarte Sena <jessicatjlopes15@gmail.com>
2024-10-30 16:45:59 -0300

    update: small adjustments

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "BlockC Api Gallery!"', () => {
+      expect(appController.get()).toBe('BlockC Api Gallery!');
     });
   });
 });
