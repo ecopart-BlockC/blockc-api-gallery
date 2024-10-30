4d1c52e38a1e53119292220b2bca654661cd7eef
Jessica Lopes Duarte Sena <jessicatjlopes15@gmail.com>
2024-10-30 16:17:52 -0300

    feat: init database config

---
+++ src/database/database.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mssql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        extra: {
+          options: {
+            applicationName: configService.get<string>('DB_APP_NAME'),
+          },
+        },
+        options: {
+          encrypt: true, 
+        },
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
