c2a2148938a90381136fd610d2e177cab734696c
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2024-10-31 15:21:37 -0300

    feat: implemented auth middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { DatabaseModule } from "./database/database.module";
 import { ConfigModule } from "@nestjs/config";
 import { UsuarioModule } from "./usuario/usuario.module";
-import { TokenModule } from './token/token.module';
+import { TokenModule } from "./token/token.module";
+import { TransferProjectModule } from "./transfer-project/transfer-project.module";
+import { AuthMiddleware } from "./auth/auth.middleware";
 
 @Module({
   imports: [
@@ -14,8 +16,13 @@ import { TokenModule } from './token/token.module';
     }),
     UsuarioModule,
     TokenModule,
+    TransferProjectModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(AppController);
+  }
+}
