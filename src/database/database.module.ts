74b970439ecc46ce9a3b1e69ae89705b9a0db531
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2024-10-31 22:24:16 -0300

    feat: added entities in database module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,6 +3,9 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { Usuario } from "src/usuario/entities/usuario.entity";
 import { Token } from "src/token/entities/token.entity";
+import { TransferProject } from "src/transfer-project/entities/transfer-project.entity";
+import { Error } from "src/error/entities/error.entity";
+import { RenewCalcProject } from "src/renew-calc-project/entities/renew-calc-project.entity";
 
 @Module({
   imports: [
@@ -19,7 +22,7 @@ import { Token } from "src/token/entities/token.entity";
         username: configService.get<string>("DB_USER"),
         password: configService.get<string>("DB_PASSWORD"),
         database: configService.get<string>("DB_DATABASE"),
-        entities: [Usuario, Token],
+        entities: [Usuario, Token, TransferProject, Error, RenewCalcProject],
         synchronize: false,
         extra: {
           options: {
