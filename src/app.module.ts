a8905133489cb48d88aa2827181efc588c888c68
Danilo Araujo <x45danilo45x@gmail.com>
2024-11-08 10:32:40 -0300

    fix: change AuthMiddleware working in all routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { MiddlewareConsumer, Module } from "@nestjs/common";
+
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { DatabaseModule } from "./database/database.module";
+import { AuthMiddleware } from "./auth/auth.middleware";
 import { ConfigModule } from "@nestjs/config";
-import { UsuarioModule } from "./usuario/usuario.module";
+import { DatabaseModule } from "./database/database.module";
+import { ErrorModule } from "./error/error.module";
+import { RenewCalcProjectModule } from "./renew-calc-project/renew-calc-project.module";
 import { TokenModule } from "./token/token.module";
 import { TransferProjectModule } from "./transfer-project/transfer-project.module";
-import { AuthMiddleware } from "./auth/auth.middleware";
-import { ErrorModule } from './error/error.module';
-import { RenewCalcProjectModule } from './renew-calc-project/renew-calc-project.module';
+import { UsuarioModule } from "./usuario/usuario.module";
 
 @Module({
   imports: [
@@ -27,6 +28,6 @@ import { RenewCalcProjectModule } from './renew-calc-project/renew-calc-project.
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthMiddleware).forRoutes(AppController);
+    consumer.apply(AuthMiddleware).forRoutes("*");
   }
 }
