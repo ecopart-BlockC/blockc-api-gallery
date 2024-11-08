4bec3765fe13713e742e25b85fd4538467660f7c
Danilo Araujo <x45danilo45x@gmail.com>
2024-11-08 15:11:46 -0300

    fix: problem with serialization to response

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,10 @@
-import { MiddlewareConsumer, Module } from "@nestjs/common";
+import {
+  ClassSerializerInterceptor,
+  MiddlewareConsumer,
+  Module,
+} from "@nestjs/common";
 
+import { APP_INTERCEPTOR } from "@nestjs/core";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthMiddleware } from "./auth/auth.middleware";
@@ -26,7 +31,10 @@ import { UsuarioModule } from "./usuario/usuario.module";
     RouteInventoryModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
+    AppService,
+  ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
