b7cee28450dde50c9c2de1562fbca9cec01d8c1e
Danilo Araujo <x45danilo45x@gmail.com>
2024-11-15 14:39:16 -0300

    fix: app routes protected with token

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,19 +8,21 @@ import { APP_INTERCEPTOR } from "@nestjs/core";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthMiddleware } from "./auth/auth.middleware";
+import { CampaignCompanyModule } from "./campaign-company/campaign-company.module";
 import { CampaignModule } from "./campaign/campaign.module";
+import { CampaignProjectModule } from "./campaign-project/campaign-project.module";
 import { ConfigModule } from "@nestjs/config";
 import { DatabaseModule } from "./database/database.module";
 import { ErrorModule } from "./error/error.module";
+import { InvNeutralizationModule } from "./inv-neutralization/inv-neutralization.module";
+import { NeutralizationModule } from "./neutralization/neutralization.module";
 import { RenewCalcProjectModule } from "./renew-calc-project/renew-calc-project.module";
 import { RouteInventoryModule } from "./route-inventory/route-inventory.module";
 import { TokenModule } from "./token/token.module";
 import { TransferProjectModule } from "./transfer-project/transfer-project.module";
-import { NeutralizationModule } from "./neutralization/neutralization.module";
-import { InvNeutralizationModule } from "./inv-neutralization/inv-neutralization.module";
 import { UsuarioModule } from "./usuario/usuario.module";
-import { CampaignProjectModule } from "./campaign-project/campaign-project.module";
-import { CampaignCompanyModule } from "./campaign-company/campaign-company.module";
+
+const APP_PROTECTED_ROUTES = "*";
 
 @Module({
   imports: [
@@ -48,6 +50,6 @@ import { CampaignCompanyModule } from "./campaign-company/campaign-company.modul
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthMiddleware).forRoutes(AppController);
+    consumer.apply(AuthMiddleware).forRoutes(APP_PROTECTED_ROUTES);
   }
 }
